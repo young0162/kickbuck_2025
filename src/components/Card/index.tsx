b231dc6391035b5fe0abe7640ae7724ee1c15afc
young <gustmdehdud@gmail.com>
2025-03-28 19:48:15 +0900

    Card 컴포넌트에 흰색 배경 수정

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = () => {
   return (
-    <div className="w-1/5 flex flex-col p-2">
+    <div className="w-1/5 flex flex-col p-2 bg-white">
       <div className="flex justify-center py-4 text-2xl my-4">제목</div>
       <div className="flex justify-between">
         <button type="button" className="w-1/2 flex justify-center text-[14px]">
